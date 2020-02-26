a0eac956ed28d031a658d6a76fb3bf3d99bc8b90
Iulian Safta <is@pix-digital.com>
2020-02-26 11:05:03 +0100

    added instalation for wkhtmltopdf

--- resources/after.sh
+++ resources/after.sh
@@ -24,3 +24,6 @@
 # Install Node.js v10.x
 #curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 #sudo apt-get install -y nodejs
+
+sudo apt-get update
+sudo apt-get install xvfb libfontconfig wkhtmltopdf
